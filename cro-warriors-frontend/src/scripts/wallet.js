54e459e6f9b71f815ad025bd342184e94ac16120
Bladerox <pa.sa95@gmx.de>
2021-09-30 02:03:44 +0200

    added combat module

--- cro-warriors-frontend/src/scripts/wallet.js
+++ cro-warriors-frontend/src/scripts/wallet.js
@@ -35,6 +35,10 @@ const availableContracts = {
     "WarriorProxy":{
         address: "0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0",
         artifact: () => import("../artifacts/WarriorProxy.json")
+    },
+    "CombatModule":{
+        address: "0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
+        artifact: () => import("../artifacts/CombatModule.json")
     }
 }
 

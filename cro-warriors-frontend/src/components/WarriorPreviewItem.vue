594f645f0b1f2de71bd03dd26923a142b5c8e049
Bladerox <pa.sa95@gmx.de>
2021-10-03 21:09:12 +0200

    Added warrior selection to menu drawer

--- cro-warriors-frontend/src/components/WarriorPreviewItem.vue
+++ cro-warriors-frontend/src/components/WarriorPreviewItem.vue
@@ -2,24 +2,32 @@
   <div>
     <v-list>
       <v-list-item>
-            <v-list-item-avatar dense>
-            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+            <v-list-item-avatar dense tile>
+            <Identicon :value="warriorDNA" v-if="warriorDNA"/>
             </v-list-item-avatar>
             
             <v-list-item-title># {{warriorID}} | {{warriorName || 'Loading...'}}</v-list-item-title>
             <v-list-item-icon>
-                <v-tooltip bottom>
+                <v-menu
+                        close-on-click
+                        
+                        >
                     <template v-slot:activator="{ on, attrs }">
                         <v-btn 
                             fab 
                             x-small 
+                            :disabled="!currentWallet"
                             elevation="0" 
                             v-bind="attrs" 
                             v-on="on"
                         ><v-icon>mdi-cog-transfer-outline</v-icon></v-btn>
                     </template>
-                    <span>Change selected warrior</span>
-                </v-tooltip>
+                    <v-card class="pa-3" tile>
+                        <v-card-title class="text-overline">Select a warrior</v-card-title>
+                        <v-divider/>
+                        <v-card-actions><ArmyList :armyAddr="currentWallet" @select="selectWarrior($event)"></ArmyList></v-card-actions>
+                    </v-card>
+                </v-menu>
             </v-list-item-icon>
       </v-list-item>
     </v-list>
@@ -27,26 +35,44 @@
 </template>
 
 <script>
+import Identicon from './Identicon.vue';
+import ArmyList from './ArmyList.vue';
+
 export default {
   name: "WarriorPreviewItem",
   props: ['warriorID'],
-  components: {},
+  components: {Identicon, ArmyList},
   methods: {
-      async bindContracts(){
+    async bindContracts(){
           if(this.$wallet.$currentWalletAddr==null){
               setTimeout(this.bindContracts, 1000);
               return;
           }
           this.$bindCall('warriorName', { contract: await this.$wallet.loadContract('WarriorVisuals'), method:"warriorName", args:[ this.warriorID ] });
+          this.$bindCall('warriorDNA', { contract: await this.$wallet.loadContract('WarriorVisuals'), method:"warriorDNA", args:[ this.warriorID ] });
+    },
+    selectWarrior(id){
+        this.$selectedWarrior = id;
+    }
+  },
+  watch: {
+      'warriorID':function(){
+          if(this.currentWallet && this.warriorID && this.warriorID.length>0){
+              this.bindContracts();
+          }
       }
   },
-  watch: {},
   mounted() {
       this.bindContracts();
   },
-  computed: {},
+  computed: {
+      currentWallet:function(){
+          return this.$wallet ? this.$wallet.$currentWalletAddr : null;
+      }
+  },
   data: () => ({
-      warriorName: null
+      warriorName: null,
+      warriorDNA: null,
   }),
 };
 </script>

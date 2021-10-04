e2eff65d863f6f897b9f44f0d7410e563135caf8
Patrick Sackschewski <pa.sa95@gmx.de>
2021-10-04 17:40:47 +0200

    added dexterity to frontend

--- cro-warriors-frontend/src/scripts/compute.js
+++ cro-warriors-frontend/src/scripts/compute.js
@@ -4,11 +4,14 @@ const healthScale = new BN(10);
 const epScale     = new BN("100000000");
 const epS1Root    = new BN("100000");
 const mintFee     = new BN("1000000000000000000");
+const critScale   = new BN("1000000000000000000");
 
 const ZERO = new BN(0);
 const ONE  = new BN(1);
 const TWO  = new BN(2);
+const TEN  = new BN(10);
 const HUNDRED  = new BN(100);
+const THOUSAND  = new BN(1000);
 
 function sqrt(x) {
     let z = x.add(ONE).div(TWO);
@@ -59,6 +62,11 @@ const compute = {
         let epNeeded = this.epForNextLevel(experience);
         experience = new BN(experience);
         return HUNDRED.mul(experience).add( epNeeded.div(TWO) ).div(epNeeded);
+    },
+    warriorCritRate(experience, dexterity){
+        let lvl = this.warriorLevel(experience);
+        dexterity = new BN(dexterity);
+        return (dexterity.mul(critScale)).div(lvl.add(TEN).mul(critScale).div(THOUSAND));
     }
 }
 

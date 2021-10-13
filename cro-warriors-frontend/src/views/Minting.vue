72b542cf1f412f27461a0443c368b9791efbd0e5
Patrick Sackschewski <pa.sa95@gmx.de>
2021-10-13 16:57:45 +0200

    added burnign functionality

--- cro-warriors-frontend/src/views/Minting.vue
+++ cro-warriors-frontend/src/views/Minting.vue
@@ -1,33 +1,43 @@
 <template>
   <v-container>
     <v-row class="text-center" justify="center">
-      <v-col md="10" lg="4">
+      <v-col md="10" lg="6">
         <MintingCard v-on:warriorMinted="updateRecentMint"/>
       </v-col>
-    </v-row>
-
-    <v-row><v-col>
       <v-expand-transition>
-        <div v-if="mostRecentMintId!==null">
-          <WarriorCard :warriorID="mostRecentMintId" />
-        </div>
+        <v-col v-if="mostRecentMintId!==null">
+            <div >
+              <WarriorCard :warriorID="mostRecentMintId" />
+            </div>
+        </v-col>
       </v-expand-transition>
-    </v-col></v-row>
+    </v-row>
+    <v-row class="text-center" justify="center">
+      <v-col md="10" lg="6">
+        <BurningCard/>
+      </v-col>
+    </v-row>
+    <v-row></v-row>
   </v-container>
 </template>
 
 <script>
 import WarriorCard from '../components/WarriorCard.vue';
 import MintingCard from '../components/MintingCard.vue';
+import BurningCard from '../components/BurningCard.vue';
   export default {
     name: 'Minting',
     components:{
       MintingCard,
+      BurningCard,
       WarriorCard
     },
     methods:{
       updateRecentMint(id){
-        this.mostRecentMintId = id;
+        setTimeout(()=>{
+          this.mostRecentMintId = id;
+        }, 1000);
+        
       }
     },
     data: () => ({
